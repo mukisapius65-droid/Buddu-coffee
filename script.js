c660ebcd8095ed3710318aa40b821d40003e8bab
iPius <mukisapius65@gmail.com>
2026-03-29 18:03:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -543,15 +543,3 @@ btns.forEach(btn => {
 
 // Set default (Uganda)
 setCountry('uganda');
-
-
-//apollo the bot
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/69c9298aecf7021c36680246/1jksskhmr';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
